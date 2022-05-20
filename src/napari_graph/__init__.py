cdbd29aab84512b8c9a30d57b85780ea91271d3e
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2022-05-20 14:24:36 -0700

    updated version import

--- src/napari_graph/__init__.py
+++ src/napari_graph/__init__.py
@@ -1,2 +1,7 @@
 from napari_graph.directed_graph import DirectedGraph
 from napari_graph.undirected_graph import UndirectedGraph
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "not-installed"
