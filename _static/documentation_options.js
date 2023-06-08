a7edb5aa0f830661a49e638e7295417273535307
jni <jni@fastmail.com>
2023-06-08 14:20:41 +0000

    Deploying to gh-pages from @ napari/napari-graph@4c47282dd4d9959be6fb77bb706158e8448d31f8 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+ge65d4eb',
+    VERSION: '0.1.dev1+g4c47282',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
