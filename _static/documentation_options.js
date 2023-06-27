a5120ed60ace9bf826a15e22b11f605163f84b3d
JoOkuma <jordao.bragantini@gmail.com>
2023-06-27 18:14:30 +0000

    Deploying to gh-pages from @ napari/napari-graph@c3bc414d121e3cddf04c6f7ad7b8ad0819c31195 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gfcc9751',
+    VERSION: '0.1.dev1+gc3bc414',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
